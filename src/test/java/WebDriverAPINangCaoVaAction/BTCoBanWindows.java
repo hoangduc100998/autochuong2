b70d98a8c650c990cfdc84ca403ca57cd88f7cc2
hoangduc <hoangduc100498@gmail.com>
2023-11-14 19:33:52 +0700

    bt

--- src/test/java/WebDriverAPINangCaoVaAction/BTCoBanWindows.java
+++ src/test/java/WebDriverAPINangCaoVaAction/BTCoBanWindows.java
@@ -1,3 +1,11 @@
+//Thực hiện truy cập "https://test.warface.codegym.vn"
+//Thực hiện mở 01 tab mới và truy cập trang "https://test.warface.codegym.vn/login"
+//Thực hiện nhập thông tin tài khoản, mật khẩu để truy cập website
+//Click đăng nhập
+//Quay về tab đầu tiên và thực hiện refresh trang này. Sau đó đóng tab đăng nhập
+//Thực hiện add 02 mặt hàng đầu tiên trong danh mục điện thoại
+//Truy cập giỏ hàng và tiến hành kiểm tra các danh sách sản phẩm
+
 package WebDriverAPINangCaoVaAction;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -36,7 +44,8 @@ public class BTCoBanWindows {
         driver.switchTo().window(driver.getWindowHandles().iterator().next());
 
         // 6. Tìm và click vào danh mục "Điện thoại"
-        WebElement dienThoaiCategory = driver.findElement(By.xpath("//a[contains(text(), 'Điện thoại')]"));
+        WebElement dienThoaiCategory =
+                driver.findElement(By.xpath("//a[contains(text(), 'Máy tính')]"));
         dienThoaiCategory.click();
 
         //7. Cuộn xuống
@@ -47,17 +56,18 @@ public class BTCoBanWindows {
         // Thêm sp 1
         driver.findElement(By.xpath("(//a[text()='Mua hàng'])[1]")).click();
         // Thêm sp 2
-        driver.findElement(By.xpath("(//a[text()='Mua hàng'])[2]")).click();
-        Thread.sleep(5000);
+        //driver.findElement(By.xpath("(//a[text()='Mua hàng'])[2]")).click();
+        //Thread.sleep(5000);
 
         // 9. Vào giỏ hàng
-        driver.findElement(By.xpath("(//a[contains(@href, 'cart')])[1]")).click();
+        //driver.findElement(By.xpath("(//a[contains(@href, 'cart')])[1]")).click();
 
         //10. Cuộn xuống
-        JavascriptExecutor js1 = (JavascriptExecutor) driver;
-        js1.executeScript("window.scrollBy(0,200)", "");
+        JavascriptExecutor js2 = (JavascriptExecutor) driver;
+        js2.executeScript("window.scrollBy(0,200)", "");
 
         //11. thoát
+        Thread.sleep(5000);
         driver.quit();
     }
 }
